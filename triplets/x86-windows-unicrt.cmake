380b9cce232cec7fe0c012cb1bf9de48e8f73cbe
MouriNaruto <Kenji.Mouri@outlook.com>
2026-04-02 15:24:12 +0800

    Add triplets\x86-windows-unicrt.cmake.

---
+++ triplets/x86-windows-unicrt.cmake
@@ -0,0 +1,14 @@
+## 
+## PROJECT:    qie-vcpkg-overlay
+## FILE:       x86-windows-unicrt.cmake
+## PURPOSE:    The vcpkg overlay triplet for Mile.Windows.UniCrt x86 target
+## 
+## LICENSE:    The MIT License
+## 
+## MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+## 
+
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_ENV_PASSTHROUGH MILE_WINDOWS_UNICRT_HOME)
