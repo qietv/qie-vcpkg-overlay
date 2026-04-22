5063c5c1d4dd8cda289c46190aec31698e02489c
MouriNaruto <Kenji.Mouri@outlook.com>
2026-04-22 19:50:56 +0800

    Add triplets\x64-android-21-static.cmake.

---
+++ triplets/x64-android-21-static.cmake
@@ -0,0 +1,17 @@
+﻿## 
+## PROJECT:    qie-vcpkg-overlay
+## FILE:       x64-android-21-static.cmake
+## PURPOSE:    The vcpkg overlay triplet for Android API Level 21 x64 target
+## 
+## LICENSE:    The MIT License
+## 
+## MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+## 
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Android)
+set(VCPKG_CMAKE_SYSTEM_VERSION 21)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-android")
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=x86_64)
