96baa629ac3ba66222e7e8b433c4a3a6ab8062a8
MouriNaruto <Kenji.Mouri@outlook.com>
2026-04-22 19:50:33 +0800

    Add triplets\arm-android-21-static.cmake.

---
+++ triplets/arm-android-21-static.cmake
@@ -0,0 +1,17 @@
+﻿## 
+## PROJECT:    qie-vcpkg-overlay
+## FILE:       arm-android-21-static.cmake
+## PURPOSE:    The vcpkg overlay triplet for Android API Level 21 ARM target
+## 
+## LICENSE:    The MIT License
+## 
+## MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+## 
+
+set(VCPKG_TARGET_ARCHITECTURE arm)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Android)
+set(VCPKG_CMAKE_SYSTEM_VERSION 21)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=armv7a-linux-androideabi")
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=armeabi-v7a -DANDROID_ARM_NEON=OFF)
