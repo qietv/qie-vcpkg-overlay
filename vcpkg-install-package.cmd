520b8136a6f6aaa5d681e145a298179bb8da36ec
MouriNaruto <Kenji.Mouri@outlook.com>
2026-04-27 14:31:59 +0800

    Add QIE_VCPKG_OUTPUT support to vcpkg-install-package.cmd.

--- vcpkg-install-package.cmd
+++ vcpkg-install-package.cmd
@@ -2,17 +2,23 @@
 
 @echo off
 
-set VCPKG_DEFAULT_BINARY_CACHE=%~dp0Output\cache
-set VCPKG_DOWNLOADS=%~dp0Output\downloads
+rem qie-vcpkg-overlay
+
+if not defined QIE_VCPKG_OUTPUT (
+    set QIE_VCPKG_OUTPUT=%~dp0Output
+)
+
+set VCPKG_DEFAULT_BINARY_CACHE=%QIE_VCPKG_OUTPUT%\cache
+set VCPKG_DOWNLOADS=%QIE_VCPKG_OUTPUT%\downloads
 set VCPKG_OVERLAY_PORTS=%~dp0ports
 set VCPKG_OVERLAY_TRIPLETS=%~dp0triplets
 
 mkdir %VCPKG_DEFAULT_BINARY_CACHE%
 
 "%~dp0vcpkg\vcpkg.exe" ^
-    "--x-buildtrees-root=%~dp0Output\buildtrees" ^
-    "--x-packages-root=%~dp0Output\packages" ^
-    "--x-install-root=%~dp0Output\installed" ^
+    "--x-buildtrees-root=%QIE_VCPKG_OUTPUT%\buildtrees" ^
+    "--x-packages-root=%QIE_VCPKG_OUTPUT%\packages" ^
+    "--x-install-root=%QIE_VCPKG_OUTPUT%\installed" ^
     install ^
     %*
 
