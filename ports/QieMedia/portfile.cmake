a77aeccbeb4cb9af24fcce00acecdfbc93161f83
MouriNaruto <Kenji.Mouri@outlook.com>
2026-06-04 17:10:23 +0800

    Add qiemedia overlay port.

---
+++ ports/QieMedia/portfile.cmake
@@ -0,0 +1,12 @@
+## 
+## PROJECT:    qie-vcpkg-overlay
+## FILE:       portfile.cmake
+## PURPOSE:    The vcpkg overlay port for pure metadata package.
+## 
+## LICENSE:    The MIT License
+## 
+## MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+## 
+
+# Just an empty port, which is used to provide metadata.
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
