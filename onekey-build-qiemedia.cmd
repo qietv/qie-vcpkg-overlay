3566c370d6de65e3fe89a6f7ad0fe05d85e839c2
MouriNaruto <Kenji.Mouri@outlook.com>
2026-07-27 14:04:56 +0800

    Update onekey-build-qiemedia.cmd.

--- onekey-build-qiemedia.cmd
+++ onekey-build-qiemedia.cmd
@@ -4,6 +4,8 @@
 
 call "%~dp0apply-patches.cmd"
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+call "%~dp0vcpkg\bootstrap-vcpkg.bat"
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 call "%~dp0vcpkg-install-package.cmd" qiemedia:arm64-windows-unicrt qiemedia:x64-windows-unicrt qiemedia:x86-windows-unicrt
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
