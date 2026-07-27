60f9fe32170f0c124af8b55b855b08d66824b4b7
MouriNaruto <Kenji.Mouri@outlook.com>
2026-07-27 14:01:29 +0800

    Update apply-patches.cmd.

--- apply-patches.cmd
+++ apply-patches.cmd
@@ -6,6 +6,10 @@ call "%~dp0reset-submodules.cmd"
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
 pushd "%~dp0vcpkg"
+git config user.name "github-actions[bot]"
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 git am --ignore-whitespace "%~dp0Patches\MileWindowsUniCrtVcpkgIntegration.patch"
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 popd
