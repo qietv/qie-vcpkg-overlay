19de4c0e5c500160995b1de0dcb50ff2141ca5b7
MouriNaruto <Kenji.Mouri@outlook.com>
2026-08-27 11:54:16 +0800

    Ensure Mile.Windows.UniCrt settings apply when reconfiguring CMake for MSVC targets.

--- Mile.Windows.UniCrt/Mile.Windows.UniCrt.cmake
+++ Mile.Windows.UniCrt/Mile.Windows.UniCrt.cmake
@@ -8,60 +8,68 @@
 ## MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
 ## 
 
-if(NOT MILE_WINDOWS_UNICRT_INCLUDED)
-    set(MILE_WINDOWS_UNICRT_INCLUDED TRUE CACHE INTERNAL "")
-    message(STATUS "Trying to configure Mile.Windows.UniCrt...")
+include_guard(GLOBAL)
 
-    if(VCPKG_TARGET_ARCHITECTURE)
-        if("${VCPKG_TARGET_ARCHITECTURE}" STREQUAL "x86")
-            set(MILE_UNICRT_LIBRARY_PLATFORM "x86")
-        elseif("${VCPKG_TARGET_ARCHITECTURE}" STREQUAL "x64")
-            set(MILE_UNICRT_LIBRARY_PLATFORM "x64")
-        elseif("${VCPKG_TARGET_ARCHITECTURE}" STREQUAL "arm64")
-            set(MILE_UNICRT_LIBRARY_PLATFORM "arm64")
-        else()
-            message(FATAL_ERROR "Unsupported VCPKG target architecture for Mile.Windows.UniCrt.")
-        endif()
-    elseif(MSVC_VERSION)
-        unset(CMAKE_MSVC_RUNTIME_LIBRARY)
-        unset(CMAKE_MSVC_RUNTIME_LIBRARY CACHE)
-        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" CACHE STRING "" FORCE)
+message(STATUS "Trying to configure Mile.Windows.UniCrt...")
 
-        include(CheckSymbolExists)
-        check_symbol_exists("_M_IX86" "" MILE_UNICRT_MSVC_TARGET_X86)
-        check_symbol_exists("_M_AMD64" "" MILE_UNICRT_MSVC_TARGET_X64)
-        check_symbol_exists("_M_ARM64" "" MILE_UNICRT_MSVC_TARGET_ARM64)
-        if(MILE_UNICRT_MSVC_TARGET_X86)
-            set(MILE_UNICRT_LIBRARY_PLATFORM "x86")
-        elseif(MILE_UNICRT_MSVC_TARGET_X64)
-            set(MILE_UNICRT_LIBRARY_PLATFORM "x64")
-        elseif(MILE_UNICRT_MSVC_TARGET_ARM64)
-            set(MILE_UNICRT_LIBRARY_PLATFORM "arm64")
-        else()
-            message(FATAL_ERROR "Unsupported MSVC target architecture for Mile.Windows.UniCrt.")
-        endif()
+if(VCPKG_TARGET_ARCHITECTURE)
+    if("${VCPKG_TARGET_ARCHITECTURE}" STREQUAL "x86")
+        set(MILE_UNICRT_LIBRARY_PLATFORM "x86")
+    elseif("${VCPKG_TARGET_ARCHITECTURE}" STREQUAL "x64")
+        set(MILE_UNICRT_LIBRARY_PLATFORM "x64")
+    elseif("${VCPKG_TARGET_ARCHITECTURE}" STREQUAL "arm64")
+        set(MILE_UNICRT_LIBRARY_PLATFORM "arm64")
     else()
-        message(FATAL_ERROR "Unsupported build system for Mile.Windows.UniCrt.")
+        message(FATAL_ERROR "Unsupported VCPKG target architecture for Mile.Windows.UniCrt.")
     endif()
+elseif(MSVC_VERSION)
+    unset(CMAKE_MSVC_RUNTIME_LIBRARY)
+    unset(CMAKE_MSVC_RUNTIME_LIBRARY CACHE)
+    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" CACHE STRING "" FORCE)
 
-    set(MILE_UNICRT_LIBRARY_PATH "${CMAKE_CURRENT_LIST_DIR}/${MILE_UNICRT_LIBRARY_PLATFORM}")
+    include(CheckSymbolExists)
 
-    if(NOT IS_DIRECTORY "${MILE_UNICRT_LIBRARY_PATH}")
-        message(FATAL_ERROR "Mile.Windows.UniCrt library not found at ${MILE_UNICRT_LIBRARY_PATH}.")
+    foreach(_MILE_UNICRT_CHECK_VARIABLE IN ITEMS
+        MILE_UNICRT_MSVC_TARGET_X86
+        MILE_UNICRT_MSVC_TARGET_X64
+        MILE_UNICRT_MSVC_TARGET_ARM64)
+        unset("${_MILE_UNICRT_CHECK_VARIABLE}")
+        unset("${_MILE_UNICRT_CHECK_VARIABLE}" CACHE)
+    endforeach()
+    unset(_MILE_UNICRT_CHECK_VARIABLE)
+
+    check_symbol_exists("_M_IX86" "" MILE_UNICRT_MSVC_TARGET_X86)
+    check_symbol_exists("_M_AMD64" "" MILE_UNICRT_MSVC_TARGET_X64)
+    check_symbol_exists("_M_ARM64" "" MILE_UNICRT_MSVC_TARGET_ARM64)
+    if(MILE_UNICRT_MSVC_TARGET_X86)
+        set(MILE_UNICRT_LIBRARY_PLATFORM "x86")
+    elseif(MILE_UNICRT_MSVC_TARGET_X64)
+        set(MILE_UNICRT_LIBRARY_PLATFORM "x64")
+    elseif(MILE_UNICRT_MSVC_TARGET_ARM64)
+        set(MILE_UNICRT_LIBRARY_PLATFORM "arm64")
+    else()
+        message(FATAL_ERROR "Unsupported MSVC target architecture for Mile.Windows.UniCrt.")
     endif()
-
-    if(VCPKG_TARGET_ARCHITECTURE)
-        string(APPEND VCPKG_LINKER_FLAGS " /LIBPATH:${MILE_UNICRT_LIBRARY_PATH}")
-        string(APPEND VCPKG_LINKER_FLAGS_DEBUG " /DEFAULTLIB:ucrtd.lib /NODEFAULTLIB:libucrtd.lib")
-        string(APPEND VCPKG_LINKER_FLAGS_RELEASE " /DEFAULTLIB:ucrt.lib /NODEFAULTLIB:libucrt.lib")
-    elseif(MSVC_VERSION)
-        add_link_options(
-            "/LIBPATH:${MILE_UNICRT_LIBRARY_PATH}"
-            "/DEFAULTLIB:ucrt$<$<CONFIG:Debug>:d>.lib"
-            "/NODEFAULTLIB:libucrt$<$<CONFIG:Debug>:d>.lib"
-        )
-    endif()
-
-    message(STATUS "Mile.Windows.UniCrt configured successfully.")
-
+else()
+    message(FATAL_ERROR "Unsupported build system for Mile.Windows.UniCrt.")
 endif()
+
+set(MILE_UNICRT_LIBRARY_PATH "${CMAKE_CURRENT_LIST_DIR}/${MILE_UNICRT_LIBRARY_PLATFORM}")
+
+if(NOT IS_DIRECTORY "${MILE_UNICRT_LIBRARY_PATH}")
+    message(FATAL_ERROR "Mile.Windows.UniCrt library not found at ${MILE_UNICRT_LIBRARY_PATH}.")
+endif()
+
+if(VCPKG_TARGET_ARCHITECTURE)
+    string(APPEND VCPKG_LINKER_FLAGS " /LIBPATH:${MILE_UNICRT_LIBRARY_PATH}")
+    string(APPEND VCPKG_LINKER_FLAGS_DEBUG " /DEFAULTLIB:ucrtd.lib /NODEFAULTLIB:libucrtd.lib")
+    string(APPEND VCPKG_LINKER_FLAGS_RELEASE " /DEFAULTLIB:ucrt.lib /NODEFAULTLIB:libucrt.lib")
+elseif(MSVC_VERSION)
+    add_link_options(
+        "/LIBPATH:${MILE_UNICRT_LIBRARY_PATH}"
+        "/DEFAULTLIB:ucrt$<$<CONFIG:Debug>:d>.lib"
+        "/NODEFAULTLIB:libucrt$<$<CONFIG:Debug>:d>.lib"
+    )
+endif()
+
+message(STATUS "Mile.Windows.UniCrt configured successfully.")
